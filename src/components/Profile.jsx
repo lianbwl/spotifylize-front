e554fa7b589dd200a2dedfef727ed9b00e1b0d15
Julian <lianvianna@gmail.com>
2019-09-30 23:57:06 -0300

    text modification

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Profile = ({ props }) => {
 	console.log(props.email);
-	return <p>Teste Viadao {props.email}</p>;
+	return <p>Email: {props.email}</p>;
 };
 
 export default Profile;
